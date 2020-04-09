b65dae4b3ef07bdf83b29dcd38606e47d81df8ac
ts <tristansetha@gmail.com>
2020-04-09 09:57:18 -0700

    3 services

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,24 @@
+package io.javabrains.moviecatalogservice.resources;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+	
+	@RequestMapping("/{userId}")
+	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+		
+		return Collections.singletonList(
+				new CatalogItem("Transformers", "Test", 4)
+		);
+		
+	}
+}
