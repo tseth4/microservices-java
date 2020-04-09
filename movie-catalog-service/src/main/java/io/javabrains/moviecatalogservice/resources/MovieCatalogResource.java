1a7c100ac4ce560b3946997e064054a35f3ba050
ts <tristansetha@gmail.com>
2020-04-09 15:17:37 -0700

    webclient method

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -10,10 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.javabrains.moviecatalogservice.models.CatalogItem;
 import io.javabrains.moviecatalogservice.models.Movie;
 import io.javabrains.moviecatalogservice.models.Rating;
+import io.javabrains.moviecatalogservice.models.UserRating;
 
 @RestController
 @RequestMapping("/catalog")
@@ -23,18 +25,30 @@ public class MovieCatalogResource {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	@Autowired
+	private WebClient.Builder webClientBuilder;
+	
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 		
+		// webclient is in the reactive space of the springboot ecosystem	
 		
-		List<Rating> ratingsList = Arrays.asList(
-				new Rating("123", 4),
-				new Rating("456", 3)
-		);
+		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
 		
-		return ratingsList.stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-			return new CatalogItem(movie.getName(), "Test", rating.getRating());
+
+		
+		return ratings.getUserRating().stream().map(rating -> {
+//			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+			
+			Movie movie = webClientBuilder.build() // webclient build patter and giving client
+				.get() // chaining mechanism to build upon this.  its a get method
+				.uri("http://localhost:8082/movies/" + rating.getMovieId()) // where do you want the request to be made
+				.retrieve() // go fetch
+				.bodyToMono(Movie.class) // whatever body you get back convert it into an instance of thie movie class. Mono is reactive way of promise
+				.block(); // tells that mono is fullfilled
+			
+			
+			return new CatalogItem(movie.getName(), "Desc", rating.getRating());
 	})
 	.collect(Collectors.toList());
 		
